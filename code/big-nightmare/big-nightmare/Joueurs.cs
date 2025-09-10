3c6aadd9a097abc4c270486762763ff6a9021378
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-09-10 14:33:28 +0200

    Feat(player) : le joueur peux bouger dans les 4 directions
    WIP

---
+++ code/big-nightmare/big-nightmare/Joueurs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace big_nightmare
+{
+    internal class Joueurs
+    {
+        private string[] view =
+        {
+            @"    ______",
+            @"  /        \",
+            @" |  @    @  |",
+            @"  \        /",
+            @"  /        \",
+            @" /          \",
+            @"/            \"
+        };
+        private int X;
+        private int Y;
+        public Joueurs() 
+        {
+            X = 20;
+            Y = 50;
+        }
+        public void update()
+        {
+            ConsoleKeyInfo keyPressed;
+            Console.Clear();
+            if (Console.KeyAvailable)
+            {
+                keyPressed = Console.ReadKey(false);
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.W:
+                        Y+=10;
+                        break;
+
+                    case ConsoleKey.A:
+                        X-=10;
+                        break;
+                    case ConsoleKey.S:
+                        Y-=10;
+                        break;
+
+                    case ConsoleKey.D:
+                        X+=10;
+                        break;
+                }
+            }
+        }
+        public void draw()
+        {
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(X, Y);
+                Console.Write(view[i]);
+            }
+        }
+    }
+}
---
+++ code/big-nightmare/big-nightmare/Program.cs
@@ -0,0 +1,47 @@
+﻿using big_nightmare;
+using System;
+using System.Numerics;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            ConsoleKeyInfo keyPressed;
+            Joueurs joueur = new Joueurs();
+            while (true)
+            {
+                Console.Clear();
+                if (Console.KeyAvailable) // L'utilisateur a pressé une touche
+                {
+                    keyPressed = Console.ReadKey(false);
+                    switch (keyPressed.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            Environment.Exit(0);
+                            break;
+
+                        case ConsoleKey.W:
+                          
+                            break;
+                    }
+                }
+                // Modifier le modèle (ce qui *est*)
+                joueur.update();
+
+                // Modifier ce que l'on *voit*
+                Console.Clear();
+                joueur.draw();
+                
+
+                // Temporiser
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
