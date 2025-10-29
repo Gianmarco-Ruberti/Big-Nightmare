160b4e19b67b0e524a66f3fdc0a71179883eb3b2
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-10-29 14:03:04 +0100

    feat(block) : modification de la création de hitbox

--- code/Drones/Model/Block.cs
+++ code/Drones/Model/Block.cs
@@ -16,117 +16,44 @@ namespace BigNightmare
             _x = x;
             _y = y;
             rotation = rota;
+            switch (rotation)
+            {
+                case 360:
+                    LeftCircle = new CircleHitbox(_x + 45, _y + 75, 35);
+                    RightCircle = new CircleHitbox(_x + 105, _y + 75, 35);
+                    break;
+                case 288:
+                    LeftCircle = new CircleHitbox(_x + 65, _y + 50, 35);
+                    RightCircle = new CircleHitbox(_x + 85, _y + 105, 35);
+                    break;
+                case 216:
+                    LeftCircle = new CircleHitbox(_x + 50, _y + 95, 35);
+                    RightCircle = new CircleHitbox(_x + 102, _y + 55, 35);
+                    break;
+                case 144:
+                    LeftCircle = new CircleHitbox(_x + 50, _y + 55, 35);
+                    RightCircle = new CircleHitbox(_x + 103, _y + 98, 35);
+                    break;
+                case 72:
+                    LeftCircle = new CircleHitbox(_x + 85, _y + 45, 35);
+                    RightCircle = new CircleHitbox(_x + 68, _y + 105, 35);
+                    break;
+            } 
+
         }
         public int X { get { return _x; } set { _x = value; } }
 
         public int Y { get { return _y; } set { _y = value; } }
-        public class CircleHitbox
-        {
-            public PointF Center { get; set; }
-            public float Radius { get; set; }
-
-            public CircleHitbox(float x, float y, float radius)
-            {
-                Center = new PointF(x, y);
-                Radius = radius;
-            }
-
-            // Vérifie si un rectangle (joueur) touche ce cercle
-            public bool collision(Rectangle rect)
-            {
-                float closestX = Math.Max(rect.Left, Math.Min(Center.X, rect.Right));
-                float closestY = Math.Max(rect.Top, Math.Min(Center.Y, rect.Bottom));
-
-                float dx = Center.X - closestX;
-                float dy = Center.Y - closestY;
-
-                return (dx * dx + dy * dy) < (Radius * Radius);
-            }
-        }
+        
 
         public CircleHitbox LeftCircle
         {
-            get
-            {
-                if (rotation == 360)
-                {
-                    float cx = _x + 45; // centre du cercle gauche
-                    float cy = _y + 75; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 288)
-                {
-                    float cx = _x + 65; // centre du cercle gauche
-                    float cy = _y + 50; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 216)
-                {
-                    float cx = _x + 50; // centre du cercle gauche
-                    float cy = _y + 95; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 144)
-                {
-                    float cx = _x + 50; // centre du cercle gauche
-                    float cy = _y + 55; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else
-                {
-                    float cx = _x + 85; // centre du cercle gauche
-                    float cy = _y + 45; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-            }
+            get; private set;           
         }
 
         public CircleHitbox RightCircle
         {
-            get
-            {
-
-                if (rotation == 360)
-                {
-                    float cx = _x + 105; // centre du cercle droit
-                    float cy = _y + 75;       // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 288)
-                {
-                    float cx = _x + 85; // centre du cercle gauche
-                    float cy = _y + 105; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 216)
-                {
-                    float cx = _x + 102; // centre du cercle gauche
-                    float cy = _y + 55; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else if (rotation == 144)
-                {
-                    float cx = _x + 103; // centre du cercle gauche
-                    float cy = _y + 98; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-                else
-                {
-                    float cx = _x + 68; // centre du cercle gauche
-                    float cy = _y + 105; // centre vertical
-                    float radius = 35;
-                    return new CircleHitbox(cx, cy, radius);
-                }
-            }
+            get; private set;
         }
 
     }
---
+++ code/Drones/Model/CircleHitbox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNightmare
+{
+    public class CircleHitbox
+    {
+        public PointF Center { get; set; }
+        public float Radius { get; set; }
+
+        public CircleHitbox(float x, float y, float radius)
+        {
+            Center = new PointF(x, y);
+            Radius = radius;
+        }
+
+        // Vérifie si un rectangle (joueur) touche ce cercle
+        public bool collision(Rectangle rect)
+        {
+            float closestX = Math.Max(rect.Left, Math.Min(Center.X, rect.Right));
+            float closestY = Math.Max(rect.Top, Math.Min(Center.Y, rect.Bottom));
+
+            float dx = Center.X - closestX;
+            float dy = Center.Y - closestY;
+
+            return (dx * dx + dy * dy) < (Radius * Radius);
+        }
+    }
+}
